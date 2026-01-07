dacbeeeca6d6cc24929f636472c0947df8991b5c
OrlandoAlata <orlandoalata@gmail.com>
2026-01-07 12:33:19 -0500

    Working through requireUsers.js

---
+++ middleware/requireUser.js
@@ -0,0 +1,25 @@
+import { findUserByToken } from "../db/auth";
+
+const requireUser = async (req, res, next) => {
+    try {
+        const auth = req.headers.authorization;
+
+        if(!auth) {
+            const error = Error("authorization required");
+            error.status = 401;
+            throw error;
+        }
+        
+        const token = auth.replace("Bearer ", "");
+
+        if(!token) {
+            const error = Error("authorization required");
+            error.status = 401;
+            throw error;
+        }
+
+        const user = await findUserByToken(token);
+
+        req.user = user;
+    }
+}
