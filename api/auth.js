d530c380a0079491badd3d629877d10c6d3f8a30
OrlandoAlata <orlandoalata@gmail.com>
2026-01-08 12:11:33 -0500

    auth.js complete under API

--- api/auth.js
+++ api/auth.js
@@ -3,3 +3,20 @@ import requireUser from ".../middleware/requireUser.js";
 import { authenticate } from "../db/auth";
 
 const router = express.Router();
+
+//Login Portion//
+router.post("/login", async (req, res, next) => {
+  try {
+    const result = await authenticate(req.body);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//This is for the specific user//
+router.get("/me", requireUser, (req, res) => {
+  res.send(req.user);
+});
+
+export default router;
