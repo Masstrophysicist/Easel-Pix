6d293a0740a8cf855708a15d313f48990ff7f6d2
Zachary <zbergman2701@gmail.com>
2026-01-11 23:53:05 -0500

    Homepage added and login began.

--- easel-pix/src/Auth/LoginForm.jsx
+++ easel-pix/src/Auth/LoginForm.jsx
@@ -1,35 +1,50 @@
 import axios from "axios";
-import { useNavigate } from "react-router";
-import "./Layout/NavBar";
 
-const LoginForm = ({ authorization }) => {
-  const navigate = useNavigate();
+const LoginForm = (authenticate) => {
   const login = async (formData) => {
-    const username = formData.get("username");
+    const email = formData.get("email");
     const password = formData.get("password");
-    const credentials = {
-      username,
+    const user = {
+      email,
       password,
     };
-    const { data } = await axios.post("/api/auth/login", credentials);
-    window.localStorage.setItem("token", data.token);
-    authorization();
-    navigate("/");
+    try {
+      const { data } = await axios.post(
+        "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login",
+        user
+      );
+      console.log(data);
+      window.localStorage.setItem("token", data.token);
+      //authenticate(window.localStorage.getItem("token"));
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   return (
     <form action={login}>
-      <h1>Login</h1>
-      <label>
-        Username:
-        <input type="text" name="username" />
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">Login</button>
+      <div>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+      </div>
+      <button type="submit"> Login </button>
     </form>
   );
 };
 
 export default LoginForm;
+
+/* Log Out Button Code:
+  Place this code under the declared component:
+    const logOut = () => {
+      setUser({});
+      };
+<button onClick={() => {logOut()}}>Log Out</button> */
---
+++ server/api/images.js
@@ -0,0 +1,7 @@
+// import express from "express";
+
+// const router = express.Router();
+
+// const staticHandler = express.static
+
+// export default router;
