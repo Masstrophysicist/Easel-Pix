9f1e9d156f0ae8ac4afb127b9cdb8620deb1cfa7
Zachary <zbergman2701@gmail.com>
2026-01-13 18:55:32 -0500

    Made major changes so that user data is gathered and sent to frontend.

--- easel-pix/src/Pages/HomePage.jsx
+++ easel-pix/src/Pages/HomePage.jsx
@@ -2,50 +2,48 @@ import { Link } from "react-router";
 import "../App.css";
 import "./userPage.css";
 import { useEffect } from "react";
+import axios from "axios";
 
-export default function HomePage({ user, setProfileData }) {
-  const bgUrl =
+export default function HomePage({ setUser, user }) {
+  const genericBanner =
     "https://www.thediscoveriesof.com/wp-content/uploads/2022/06/Mountain-Landscape-in-Colorado-Rocky-Mountains-Colorado-United-States..jpg.webp";
-  const profileUrl =
+  const genericProfile =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSalPUqb1gpbBibzSAKkG_3QISmsftnXoURZEc4LCnudqiy3mazEuW48k1eBclvAs75oT0SWbRGmOdHVIBUhtYIGdCC7oqOsTz0qA8nPA&s=10";
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
+        const response = await axios.get("/me");
+        console.log("Hello User", response);
+        const fetchedUser = response.user;
+        setUser((prevUser) => ({ ...prevUser, ...fetchedUser }));
       } catch (error) {
         console.log("Error fetching user", error.message);
       }
     };
 
-    fetchUser;
-
-    const fetchImages = async () => {
-      try {
-        const response = await axios.get("/api/images");
-        setProfileData({
-          backgroundUrl: response.data.bannerPicture,
-          profilePicUrl: response.data.profilePicture,
-        });
-      } catch (error) {
-        console.error("Error fetching images", error.message);
-      }
-    };
-
-    fetchImages();
+    fetchUser();
   }, []);
 
   return (
     <div>
       <div
         className="backgroundPic"
-        style={{ backgroundImage: `url(${bgUrl})` }}
+        style={{
+          backgroundImage: `url(${
+            user?.bannerPicture ? user.bannerPicture : genericBanner
+          })`,
+        }}
       >
         <div
           className="profilePicBorder"
-          style={{ backgroundImage: `url(${profileUrl})` }}
+          style={{
+            backgroundImage: `url(${
+              user?.profilePicture ? user.profilePicture : genericProfile
+            })`,
+          }}
         ></div>
-        <div className="profilePic"></div>
-        <div className="profileName">John Doe</div>
+        <div className="profileName">{user?.displayname}</div>
       </div>
 
       <div className="posts">
